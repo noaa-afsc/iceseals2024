4126d18eee4aa02d93009d1a6ca4902648af125e
Josh M. London <josh.london@noaa.gov>
2024-05-17 13:09:53 -1000

    troubleshooting ghaction

--- R/get_wc_data.R
+++ R/get_wc_data.R
@@ -3,28 +3,28 @@ get_wc_data <- function(deployids) {
   ids <- purrr::map_dfr(
     deployids,~ wcUtils::wcGetDeployID(wcUtils::wcPOST(), 
                                                  deployid = .x)
-  ) |> pull(ids)
+  ) |> dplyr::pull(ids)
   
   res <- purrr::map(ids, wcGetDownload)
   
   all_locs_tbl <- purrr::map(res,"all_locations") %>% 
-    bind_rows() %>% 
-    drop_na(any_of(c("latitude", "longitude")))
+    dplyr::bind_rows() %>% 
+    dplyr::drop_na(any_of(c("latitude", "longitude")))
   
   locs_tbl <- purrr::map(res,"locations") %>% 
-    bind_rows() %>% 
-    drop_na(any_of(c("latitude", "longitude")))
+    dplyr::bind_rows() %>% 
+    tidyr::drop_na(any_of(c("latitude", "longitude")))
   
-  locs_tbl <- bind_rows(locs_tbl,all_locs_tbl)
+  locs_tbl <- dplyr::bind_rows(locs_tbl,all_locs_tbl)
   
   ecdf_tbl <- purrr::map(res,"ecdf") %>% 
-    bind_rows()
+    dplyr::bind_rows()
   
   pdt_tbl <- purrr::map(res,"pdt") %>% 
-    bind_rows()
+    dplyr::bind_rows()
   
   behav_tbl <- purrr::map(res,"behavior") |> 
-    bind_rows()
+    dplyr::bind_rows()
   
   histos <- purrr::map(res,"histos")
   
@@ -33,10 +33,10 @@ get_wc_data <- function(deployids) {
   histos <- histos[!null_histos]
   
   tad_tbl <- purrr::map(histos,tidyTimeAtDepth) |> 
-    bind_rows()
+    dplyr::bind_rows()
   
   haul_out <- purrr::map(histos,tidyTimelines) |> 
-    bind_rows()
+    dplyr::bind_rows()
   
   return(list(locs=locs_tbl,ecdf=ecdf_tbl,pdt=pdt_tbl,behav=behav_tbl,
               tad=tad_tbl,haul_out=haul_out))
