4bd6bcdaa9548b2e5918d4e023fad8d720fea608
Josh M. London <josh.london@noaa.gov>
2024-05-18 11:14:36 -1000

    add ship track to map

---
+++ scripts/get_ship_track.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(sf)
+library(here)
+
+cruise_bbox <- st_bbox(c(xmin = -166.5, 
+                         xmax = -179.9, 
+                         ymax = 63, 
+                         ymin = 53.9), 
+                       crs = st_crs(4326)) |> 
+  st_as_sfc()
+
+
+ship_track_pts <- sf::st_read(here::here('data/export_05182024.kml')) |> 
+  sf::st_filter(cruise_bbox)
+  
+
+ship_track <- ship_track_pts |> 
+  summarise(do_union = FALSE) |> 
+  st_cast("LINESTRING")
+
+readr::write_rds(ship_track,here::here('data/ship_track.rds'))
