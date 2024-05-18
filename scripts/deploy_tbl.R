1346110c071cc1cc8326d9f66cafa437825695bf
Josh M. London <josh.london@noaa.gov>
2024-05-17 23:09:35 -1000

    add summary table + other enhancements

--- scripts/deploy_tbl.R
+++ scripts/deploy_tbl.R
@@ -12,3 +12,9 @@ read_sheet(
          speno = animal_tag_id) %>% 
   mutate(deploy_date_time_gmt = lubridate::force_tz(deploy_date_time_gmt,"UTC")) %>% 
   readr::write_rds(file = here::here('data/deploy_tbl.rds'))
+
+read_sheet(
+  "1zt_rtQFaEQluoJuyBkcIQvNSkFWVpgtbOWKuFd9Z2uc"
+) %>% 
+  janitor::clean_names() %>% 
+  readr::write_rds(file = here::here('data/summary_tbl.rds'))
